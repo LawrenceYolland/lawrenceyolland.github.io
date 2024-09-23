77b5fb6bc3a860e5ba9868fb71bdc1cfc09dce38
eyollaw <lawrence.yolland@redbeemedia.com>
2024-09-23 23:13:03 +0100

    add vite and deploy workflow

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      </div>
+      <h1>Lawrence Yolland</h1>
+      <div>
+        <h2>🚧 Under construction 🚧</h2>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
